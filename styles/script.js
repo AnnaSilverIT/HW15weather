8171e63b1395604a1b0a5c68418e9bc0ccdbc55e
AnnaSilverIT <student291297@gmail.com>
2024-09-22 21:09:10 +0300

    fix java script

--- styles/script.js
+++ styles/script.js
@@ -5,46 +5,50 @@ const degrees = new Array(6)
 
 
 
-for (let i = 0; i < degrees.length; i += 1) { degrees[i] = prompt()};
-    let max = degrees[0];
-    for (let i = 1; i < degrees.length; i++) {
-    if (degrees[i] > max) {
-    max = degrees[i];
-    }
+for (let i = 0; i < degrees.length; i+= 1) { 
+  degrees[i] = +prompt(`Введите температуру для города ${cities[i]}`)
+};
+function max(list) {
+  let max = list[0];
+  list.forEach(function(e) {
+  if (e > max) max = e;
+  });
+  return max;
   }
-console.log(max)
-let min = degrees[0];
-for (let i = 1; i < degrees.length; i++) {
-if (degrees[i] < min) {
-min = degrees[i];
-}
-}
-console.log(min) 
+  function min(list) {
+    let min = list[0];
+    list.forEach(function(e) {
+    if (e < min) min = e;
+    });
+    return min;
+    }
+  
+console.log(degrees)
 
 
 let firstCity = document.createElement('p');
-firstCity.innerText = 'Погода в Москве: ' + degrees[0]
+firstCity.innerText = 'Погода в Москве: ' + degrees[0] + ' градусов.'
 wrapper.appendChild(firstCity)
 let secondCity = document.createElement('p');
-secondCity.innerText = 'Погода в Ижевске: ' +  degrees[1]
+secondCity.innerText = 'Погода в Ижевске: ' +  degrees[1] + ' градусов.'
 wrapper.appendChild(secondCity)
 let thirstCity = document.createElement('p');
-thirstCity.innerText = 'Погода в Санкт-Петербурге: ' + degrees[2]
+thirstCity.innerText = 'Погода в Санкт-Петербурге: ' + degrees[2] + ' градусов.'
 wrapper.appendChild(thirstCity)
 let fourCity = document.createElement('p');
-fourCity.innerText = 'Погода в Калининграде: ' + degrees[3]
+fourCity.innerText = 'Погода в Калининграде: ' + degrees[3] + ' градусов.'
 wrapper.appendChild(fourCity)
 let fithCity = document.createElement('p');
-fithCity.innerText = 'Погода в Сочи : ' + degrees[4]
+fithCity.innerText = 'Погода в Сочи : ' + degrees[4] + ' градусов.'
 wrapper.appendChild(fithCity)
 let sixthCity = document.createElement('p');
-sixthCity.innerText = 'Погода в Перми: ' + degrees[5]
+sixthCity.innerText = 'Погода в Перми: ' + degrees[5] + ' градусов.'
 wrapper.appendChild(sixthCity)
 let maxDegrees = document.createElement('p');
-maxDegrees.innerText = 'Максимальная Температура: ' + max
+maxDegrees.innerText = 'Максимальная температура: ' + max(degrees) + ' градусов.'
 wrapper.appendChild(maxDegrees)
 let minDegrees = document.createElement('p');
-minDegrees.innerText = 'Минимальная температура: ' + min
+minDegrees.innerText = 'Минимальная температура: ' + min(degrees) + ' градусов.'
 wrapper.appendChild(minDegrees)
 
 
